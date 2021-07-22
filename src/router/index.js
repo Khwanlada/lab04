ab794439cbc6d4d6233d26fc228e2d496af109db
Khwanlada <khwanlada_k@cmu.ac.th>
2021-07-23 00:54:29 +0700

    2.9 Finish

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
 import About from '../views/About.vue'
+import EventDetails from '@/views/EventDetails.vue'
+
 const routes = [
   {
     path: '/',
     name: 'EventList',
-    component: EventList
+    component: EventList,
+    props:(route) => ({ perPage: parseInt(route.query.perPage) || 2 , page: parseInt(route.query.page) || 1  })
   },
   {
     path: '/about',
     name: 'About',
     component: About
+  },
+  {
+    path: '/event/:id',
+    name:'EventDetails',
+    component:EventDetails,
+    props: true
   }
 ]
 
